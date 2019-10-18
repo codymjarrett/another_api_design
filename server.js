608b3d35a1157148e6667499a9658ad1bec9b94e
Cody Jarrett <cody.jarrett@americaneagle.com>
2019-10-18 08:29:25 -0500

    db connection, added colors package, added data seeds

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
 const logger = require('./middleware/logger')
+const connectDB = require('./config/db')
+
+// load config file
+dotenv.config({ path: './config/config.env' })
+
+//connect to DB
+connectDB()
 
 //route files
 const bootcamps = require('./routes/bootcamps')
 
-// load config file
-dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
@@ -24,8 +30,16 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log(
-		`listening in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+		`listening in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`.magenta.bold
 	)
+})
+
+// handle unhandled proise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red.bold)
+	// close server and exit process
+	server.close(()=> process.exit(1))
 })
